caf987280bf7ffacf41660bc3f6eabace913c904
yassinouk <yassin.ouakka@etu.uae.ac.ma>
2023-12-04 01:49:50 +0100

    adding site map and robots.txt file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()]
+  site: 'https://www.samanhalagarden.ma',
+  integrations: [tailwind(), vue(), sitemap()]
 });
